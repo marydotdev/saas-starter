1913dd41f39fc4a5c03515cf535c739f624f88cc
marydotdev <mary.haedrich@gmail.com>
2025-06-04 12:55:06 -0400

    Fix Stripe API version and Supabase config

---
+++ lib/db/supabase/client.ts
@@ -0,0 +1,34 @@
+import {
+  createClientComponentClient,
+  createServerComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { createClient } from "@supabase/supabase-js";
+import { cookies } from "next/headers";
+
+// Client-side Supabase client
+export const createClientSupabase = () =>
+  createClientComponentClient({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  });
+
+// Server-side Supabase client
+export const createServerSupabase = () =>
+  createServerComponentClient({
+    cookies,
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  });
+
+// Admin Supabase client
+export const createAdminSupabase = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    }
+  );
